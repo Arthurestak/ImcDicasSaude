29175669cb35044be5ca0d432a5fa6c928000cca
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2024-05-15 15:30:26 -0300

    Create ImcDicasSaude.js

---
+++ ImcDicasSaude.js
@@ -0,0 +1,28 @@
+const peso = 100,
+
+const altura = 1.54;
+
+const imc peso / (altura**2);
+
+const perderPeso = 'Você deve optar por una dieta mais saudável, visando eliminar o excesso de carboidratos e alimentos con alto tour caldrion
+
+const obesidade = 'Obesidade morbida!! Você deve optar por una dieta nais saudável, eliminando o excesso de carboidratos e alimentus cas alto const ganharPeso 'Você precisa adotar una dieta onde mesmo sen querer você precisa ingerir alimentos mais do que o normal, alimentos rices a
+
+const ganharMassa = 'Deve-se optar por uma rotina nais voltada à alimentação com muita proteina e muito carboidrato, ovos a carnes são muito
+
+if (imc <= 18.5)(
+
+ console.log ('Seu inc & de', imc.toFixed(2), '!', ganharMassa)
+
+else if(imc >= 18.6 88 imc <=24.9){
+
+    console.log ('Seu inc é de', imc.toFixed(2), '! Você está no peso ideal para a sua altura!')
+
+  else if(imc >= 25 48 imc <= 29.9)(
+
+    console.log ('Seu inc é de', imc.toFixed(2), '!', perderPeso)
+
+  else if(imc > 30)(
+
+console.log ('Seu imc é de', imc.toFixed(2), '!', obesidade)
+
